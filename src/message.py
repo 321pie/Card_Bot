bd027850cfd118c8e28516f152aadbe38749f816
321pie <aprspie@gmail.com>
2025-01-06 09:59:59 -0800

    Fixed misleading typo

--- src/message.py
+++ src/message.py
@@ -67,7 +67,7 @@ async def handle_user_messages(msg):
         return make_cribbage(player)
     elif message == "!juiced" or message == "!jc":
         return make_juiced(player)
-    elif message == "!test" or message == "!jc":
+    elif message == "!test":
         return make_test(player)
     
     #Roles
